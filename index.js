60be1aa16a29167b98483b13eae0dc8d65e0e58b
Ho Minh <Minhho.technology@gmail.com>
2022-01-03 13:45:30 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
+
+const app = express();
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("DB connecting Successfull");
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//routes
+// app.use("/api/auth", authRouter);
+// app.use("/api/users", usersRouter);
+// app.use("/api/movies", moviesRouter);
+// app.use("/api/lists", listsRouter);
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server is running port ${port}`);
+});
